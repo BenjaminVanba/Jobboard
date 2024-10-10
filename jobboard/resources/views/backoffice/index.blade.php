81260a8a67280e2d83616602adc89b15cb428688
Vanbaelinghem <benjamin.vanbaelinghem@epitech.eu>
2024-10-10 15:05:26 +0200

    changements

--- jobboard/resources/views/backoffice/index.blade.php
+++ jobboard/resources/views/backoffice/index.blade.php
@@ -8,7 +8,7 @@
     <h1 class="text-3xl font-bold mb-5">Tableau de bord du Backoffice</h1>
     
     <ul class="list-disc pl-5">
-        <li><a href="{{ route('backoffice_annonces') }}" class="text-blue-500">Créer une nouvelle annonce</a></li>
+        <li><a href="{{ route('backoffice_annonces') }}" class="text-blue-500">Voir les annonces</a></li>
         {{-- <li><a href="{{ route('advertisement.index') }}" class="text-blue-500">Lister toutes les annonces</a></li> --}}
         <li><a href="{{ route('companies') }}" class="text-blue-500">Gérer les entreprises</a></li>
         <!-- Ajoutez d'autres liens vers d'autres pages du backoffice -->
